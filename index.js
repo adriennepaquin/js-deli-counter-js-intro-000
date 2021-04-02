69cf5e96304513399d87506c3f4031224bc7ba03
Adrienne Paquin <adriennepaquin@gmail.com>
2021-04-02 16:06:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,10 +29,14 @@ function currentLine(katzDeliLine) {
     return "The line is currently empty."
   }
   else {
-    return "The line is currently: ";
     for (let i = 0; i < katzDeliLine.length; i++) {
       //console.log((i + 1) + ". " + katzDeliLine[i]);
-      return (i + 1) + ". " + katzDeliLine[i];
+      if (i === 0) {
+          return "The line is currently: 1. " + katzDeliLine[i];
+      }
+      else {
+        return ", " + (i + 1) + ". " + katzDeliLine[i];
+      }
     }
   }
 }
